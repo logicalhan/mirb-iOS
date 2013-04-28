d4bbd75dcc8d009b8bcd3e703623cd342c651032
Justin Mazzocchi <jzzocc@gmail.com>
2013-04-28 12:43:17 -0700

    Initial Commit

---
+++ mirb/MBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MBAppDelegate.h
+//  mirb
+//
+//  Created by Justin Mazzocchi on 2013-04-28.
+//  Copyright (c) 2013 Justin Mazzocchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mirb/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mirb
+//
+//  Created by Justin Mazzocchi on 2013-04-28.
+//  Copyright (c) 2013 Justin Mazzocchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MBAppDelegate class]));
+    }
+}
